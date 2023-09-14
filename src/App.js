e53ea785ce0724b584de6ce7ccf7bc549c43167d
Ashique <ashiqurrahman2092003@gmail.com>
2023-09-14 23:45:18 +0600

    Add new feature: login functionality

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Cars from './componenets/cars-conteiner/cars';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App ">
       <h1>Confused Buyer</h1>
       <h2>Choose 4 Cars</h2>
       <div>
